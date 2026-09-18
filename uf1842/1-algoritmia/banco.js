cbc78f35dc8a804830fc9af4e04300a043045c4d
mcmamen71 <mc.mamen@hotmail.com>
2026-09-18 18:49:45 +0200

    Ejemplo de examen

--- uf1842/1-algoritmia/banco.js
+++ uf1842/1-algoritmia/banco.js
@@ -14,7 +14,11 @@
  */
 
 function gestionarCuenta(saldo, operacion, cantidad) {
-
+if (operacion === "ingresar") {
+    return saldo + cantidad;
+  } else if (operacion === "retirar") {
+    return saldo - cantidad;
+  }
 }
 
 // Prueba 1: Ingresar dinero
--- uf1842/2-DOM/index.js
+++ uf1842/2-DOM/index.js
@@ -1,1 +1,13 @@
 // ¡Buena suerte!
+const billInput = document.querySelector("#bill");
+const tipInput = document.querySelector("#tip");
+const calculateBtn = document.querySelector("#calculate");
+const totalAmount = document.querySelector("#total");
+
+calculateBtn.addEventListener("click", function() {
+ const bill = parseFloat(billInput.value) || 0;
+    const tip = parseFloat(tipInput.value) || 0;
+    const tipAmount = bill * (tip / 100);
+    const total = bill + tipAmount; 
+    totalAmount.textContent = total.toFixed(2);  
+});
